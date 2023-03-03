aeba3a3dcbad9e40118fecd477c92404ee937d5b
Ksawery Szewczyk <ksawery.szewczyk2@gmail.com>
2023-03-03 15:45:11 +0100

    feat: add basic index page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,53 @@
+<script lang="ts">
+	import '../index.css';
+</script>
+
 <div>
-    <h1>sushi</h1>
+	<span>
+		<h1>sushi</h1>
+		<p>Use functions from any language, in any language.</p>
+	</span>
 </div>
+
+<style>
+	@import 'https://rsms.me/inter/inter.css';
+
+	:root {
+		font-family: 'Inter', sans-serif;
+	}
+
+	@supports (font-variation-settings: normal) {
+		:root {
+			font-family: 'Inter var', sans-serif;
+		}
+	}
+
+	:global(body) {
+		background: url('../img/noise.png');
+		background-color: #121212;
+	}
+
+	div {
+		width: 100%;
+		height: 100%;
+
+		display: grid;
+		place-items: center;
+		position: fixed;
+	}
+
+	h1,
+	p {
+		color: white;
+	}
+
+	h1 {
+		font-weight: 800;
+		font-size: 3em;
+	}
+
+	p {
+		margin-top: 10px;
+		white-space: pre-line;
+	}
+</style>
